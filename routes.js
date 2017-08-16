4136eceeffb3a26cb307b68f2e9fdc2fee983d64
moeabdol <mohd.a.saed@gmail.com>
2017-08-16 21:21:10 +0300

    Add signup routes

--- routes.js
+++ routes.js
@@ -1,6 +1,7 @@
-const express = require("express");
-const path    = require("path");
-const User    = require(path.resolve(__dirname, "models", "user"));
+const express  = require("express");
+const passport = require("passport");
+const path     = require("path");
+const User     = require(path.resolve(__dirname, "models", "user"));
 
 const router = express.Router();
 
@@ -19,5 +20,32 @@ router.get("/", (req, res, next) => {
     });
   });
 });
+
+router.get("/signup", (req, res) => {
+  res.render("signup");
+});
+
+router.post("/signup", (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({ username: username }, (err, user) => {
+    if (err) { return next(err); }
+    if (user) {
+      req.flash("error", "User already exists");
+      return res.redirect("/signup");
+    }
+
+    const newUser = new User({
+      username: username,
+      password: password
+    });
+    newUser.save(next);
+  });
+}, passport.authenticate("login", {
+  successRedirect: "/",
+  failureRedirect: "/signup",
+  failureFlash:    true
+}));
 
 module.exports = router;
